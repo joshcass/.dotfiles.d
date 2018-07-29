090f5b075a74e9df36dfff0ad5b343fba87e77ac
Josh Cass <hello@joshcass.com>
2018-07-29 14:55:23 -0600

    install the correct yarn

--- setup/ppas.sh
+++ setup/ppas.sh
@@ -23,5 +23,17 @@ install_keybase() {
     fi
 }
 
+install_yarn() {
+    if [ ! -x "$(command -v yarn)" ]; then
+        curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+        echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+        sudo apt update
+        sudo apt install yarn -y
+    else
+        echo "Yarn already installed"
+    fi
+}
+
 install_parity
 install_keybase
+install_yarn
