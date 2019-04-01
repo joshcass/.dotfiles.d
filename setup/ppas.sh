db758f201f68404c888888e7ae45a3d0cea5a341
Josh Cass <hello@joshcass.com>
2019-04-01 13:53:56 -0600

    add insync and use apt-get > apt

--- setup/ppas.sh
+++ setup/ppas.sh
@@ -3,8 +3,8 @@ install_parity() {
         echo "Installing Thoughtbot Parity"
         wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
         echo "deb http://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
-        sudo apt update
-        sudo apt install parity -y
+        sudo apt-get update
+        sudo apt-get install parity -y
     else
         echo "Thoughbot Parity already installed"
     fi
@@ -27,8 +27,8 @@ install_yarn() {
     if [ ! -x "$(command -v yarn)" ]; then
         wget -qO https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
         echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-        sudo apt update
-        sudo apt install yarn -y
+        sudo apt-get update
+        sudo apt-get install yarn -y
     else
         echo "Yarn already installed"
     fi
@@ -38,10 +38,21 @@ install_chrome() {
     if [ ! -x "$(command -v google-chrome)" ]; then
         wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
         echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
-        sudo apt update
-        sudo apt intsall google-chrome-stable -y
+        sudo apt-get update
+        sudo apt-get install google-chrome-stable -y
     else
         echo "Google Chrome already installed"
+    fi
+}
+
+install_insync() {
+    if [ ! -x "$(command -v insync)" ]; then
+        sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
+        echo deb http://apt.insynchq.com/ubuntu "$(lsb_release -cs)" non-free contrib | sudo tee /etc/apt/sources.list.d/insync.list
+        sudo apt-get update
+        sudo apt-get install insync -y
+    else
+        echo "Insync already installed"
     fi
 }
 
@@ -49,3 +60,4 @@ install_parity
 install_keybase
 install_yarn
 install_chrome
+install_insync
