370296cb14eb290965627ffb6ea3779ffdfb64c1
Josh Cass <hello@joshcass.com>
2020-06-20 14:00:00 -0600

    some combination of ctrl:nocaps and switching kb layouts breaks a lot of things so just going back to interception tools

---
+++ setup.d/caps2esc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+https://gitlab.com/interception/linux/plugins/caps2esc
+
+echo "Writing udevmon job configuration"
+cat <<-EOF | sudo tee /etc/udevmon.yaml
+- JOB: "intercept -g \$DEVNODE | caps2esc | uinput -d \$DEVNODE"
+  DEVICE:
+    EVENTS:
+      EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+EOF
+
+echo "Enabling udevmon service"
+sudo systemctl enable udevmon --now
