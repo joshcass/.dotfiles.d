a8dfc09fcf661bbd2345fbd24047c0b881a9b1e6
Josh Cass <hello@joshcass.com>
2020-09-26 00:22:57 -0600

    remove copy of pure

--- fish/functions/theme-pure/functions/_pure_prompt_current_time.fish
+++
@@ -1,8 +0,0 @@
-function _pure_prompt_current_time \
-    --description 'Print current time'
-
-    set --local time_color (_pure_set_color $pure_color_warning)
-    set --local current_time (date +%H:%M)
-
-    echo "$time_color$current_time"
-end
