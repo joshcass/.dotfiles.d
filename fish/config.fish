fe25399648d3e567049a936dbc6fc38e5d35bfdf
Josh Cass <hello@joshcass.com>
2020-09-26 00:16:11 -0600

    update config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 # THEME PURE #
-set fish_function_path $HOME/.config/fish/functions/theme-pure/functions/ $fish_function_path
-source $HOME/.config/fish/functions/theme-pure/conf.d/pure.fish
+set fish_function_path $HOME/.config/fish/functions/pure/functions/ $fish_function_path
+set fish_function_path $HOME/.config/fish/functions/custom_pure/ $fish_function_path
+source $HOME/.config/fish/functions/pure/conf.d/pure.fish
+
+# Z #
+set fish_function_path $HOME/.config/fish/functions/z/functions/ $fish_function_path
+source $HOME/.config/fish/functions/z/conf.d/z.fish
