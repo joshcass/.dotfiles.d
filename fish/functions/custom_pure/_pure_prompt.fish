2c9e1d47f6264d4fdf692682169e4f7e082d53f7
Josh Cass <hello@joshcass.com>
2020-09-26 00:15:45 -0600

    custom pure functions

---
+++ fish/functions/custom_pure/_pure_prompt.fish
@@ -0,0 +1,12 @@
+function _pure_prompt \
+    --description 'Print prompt symbol' \
+    --argument-names exit_code
+
+    set --local jobs (_pure_prompt_jobs)
+    set --local virtualenv (_pure_prompt_virtualenv) # Python virtualenv name
+    set --local vimode_indicator (_pure_prompt_vimode) # vi-mode indicator
+    set --local current_time (_pure_prompt_current_time)
+    set --local pure_symbol (_pure_prompt_symbol $exit_code)
+
+    echo (_pure_print_prompt $jobs $virtualenv $vimode_indicator $current_time $pure_symbol)
+end
