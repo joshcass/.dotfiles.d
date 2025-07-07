782170ee738e58f04e46e688475af8d09a5e07ee
Josh Cass <hello@joshcass.com>
2025-07-07 16:00:24 -0700

    less like tree more like ranger

--- conf.d/config/nvim/lua/plugins/snacks.lua
+++ conf.d/config/nvim/lua/plugins/snacks.lua
@@ -2,5 +2,17 @@ return {
   "folke/snacks.nvim",
   opts = {
     indent = { enabled = false },
+    explorer = {
+      replace_netrw = true, -- Replace netrw with the snacks explorer
+    },
+    picker = {
+      sources = {
+        explorer = {
+          tree = false,
+          auto_close = true,
+          layout = { preset = "default", preview = true },
+        },
+      },
+    },
   },
 }
