b561abbc2e568ccdaf008191ecd21e7150f35aae
Josh Cass <hello@joshcass.com>
2026-03-08 12:50:38 -0700

    add back gpg auth

--- conf.d/config/fish/conf.d/00_env.fish
+++ conf.d/config/fish/conf.d/00_env.fish
@@ -10,6 +10,10 @@ set -gx Z_DATA_DIR $HOME/.local/share/z
 
 set -gx Z_EXCLUDE \x5e$HOME\x24
 
+set -gx GPG_TTY (tty)
+
+set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
 set fish_function_path $HOME/.config/fish/functions/pure/functions $fish_function_path
 
 set fish_function_path $HOME/.config/fish/functions/z/functions $fish_function_path
